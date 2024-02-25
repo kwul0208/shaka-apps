79687005544521312390e492a49ab916587c37f5
kwul0208 <kwul0208@gmail.com>
2024-02-25 15:12:34 +0700

    feat: chat example design:

--- lib/features/chats/presentation/pages/chats.dart
+++ lib/features/chats/presentation/pages/chats.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shaka/constants/constants.dart';
+import 'package:shaka/constants/responsive.dart';
 
 class Chat extends StatelessWidget {
   const Chat({super.key});
@@ -9,6 +10,7 @@ class Chat extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
@@ -38,7 +40,114 @@ class Chat extends StatelessWidget {
                     SizedBox(height: appPadding,),
                     Text("ChatGPT", style: TextStyle(fontWeight: FontWeight.w500, fontSize: 18),),
                     SizedBox(height: appPadding,),
-                    Text("Discover the revolutionary power of Open A I Gpt 3.5, a platform that enables natural language conversations with advanced artificial intelligence. Engage in dialogue, ask questions, and receive intelligent responses to enhance your interactive communication experience.", style: TextStyle(color: lightTextColor), textAlign: TextAlign.center,)
+                    Text("Discover the revolutionary power of Open AI Gpt 3.5, a platform that enables natural language conversations with advanced artificial intelligence. Engage in dialogue, ask questions, and receive intelligent responses to enhance your interactive communication experience.", style: TextStyle(color: lightTextColor), textAlign: TextAlign.center,),
+                    SizedBox(height: 20,),
+                    InkWell(
+                      borderRadius: BorderRadius.all(Radius.circular(6)),
+                      onTap: (){
+                        showModalBottomSheet<void>(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+                          ),
+                          context: context,
+                          isScrollControlled: true, // set this to true
+                          builder: (context) {
+                            return StatefulBuilder(
+                              builder: (BuildContext context, StateSetter setState ) {
+                                return Material(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(appPadding),
+                                  child: DraggableScrollableSheet(
+                                    initialChildSize: 0.8,
+                                    minChildSize: 0.25,
+                                    maxChildSize: 0.97,
+                                    expand: false,
+                                    builder: (context, scrollController) {
+                                      return Column(
+                                        children: <Widget>[
+                                          Container(
+                                            // color: Colors.white,
+                                            width: Responsive.screen_width(context),
+                                            height: 80,
+                                            child: ListView(
+                                              controller: scrollController,
+                                              children: <Widget>[
+                                                Align(
+                                                  alignment: Alignment.topCenter,
+                                                  child: Container(
+                                                    height: 5.0,
+                                                    width: 70.0,
+                                                    decoration: BoxDecoration(
+                                                      color: Colors.grey[400],
+                                                      borderRadius: BorderRadius.circular(10.0),
+                                                    ),
+                                                  ),
+                                                ),
+                                                SizedBox(height: 16),
+                                                Padding(
+                                                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                                                  child: Row(
+                                                    mainAxisAlignment: MainAxisAlignment.center,
+                                                    children: [
+                                                      Text('Examples', style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500)),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ),
+                                          Expanded(
+                                            child: ListView.builder(
+                                              itemCount: 10,
+                                              itemBuilder: (context, index) => Padding(
+                                                padding: const EdgeInsets.symmetric(horizontal: appPadding, vertical: appPadding/2),
+                                                child: Container(
+                                                  decoration: BoxDecoration(
+                                                    color: bgColor,
+                                                    borderRadius: BorderRadius.circular(appPadding)
+                                                  ),
+                                                  child: Padding(
+                                                    padding: const EdgeInsets.all(appPadding),
+                                                    child: Column(
+                                                      children: [
+                                                        Text("Title", style: TextStyle(fontWeight: FontWeight.w500,  fontSize: 18),),
+                                                        Text("subTitle aasad sd sd asa", style: TextStyle(color: lightTextColor,  fontSize: 14),)
+                                                      ],
+                                                    ),
+                                                  ),
+                                                ),
+                                              )
+                                            ),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  ),
+                                );
+
+                              }
+                            );
+                          },
+                        );
+                      },
+                      child :Container(
+                        width: 100,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(6)),
+                          border: Border.all(color: lightTextColor)
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(4),
+                          child: Row(
+                            children: [
+                              Icon(Icons.wb_incandescent_sharp, size: 18, ),
+                              SizedBox(width: 4,),
+                              Text("Examples", style: TextStyle(fontWeight: FontWeight.w500),),
+                            ],
+                          ),
+                        ),
+                      )
+                    )
                   ],
                 ),
               ),
@@ -48,7 +157,7 @@ class Chat extends StatelessWidget {
         ],
       ),
     );
-  }
+  }                                     
 }
 
 class _MessageBar extends StatefulWidget {
@@ -82,7 +191,7 @@ class _MessageBarState extends State<_MessageBar> {
                       keyboardType: TextInputType.multiline,
                       maxLines: 5,
                       minLines: 1,
-                      autofocus: true,
+                      autofocus: false,
                       controller: _textController,
                       decoration: const InputDecoration(
                         hintText: 'Type a message',
